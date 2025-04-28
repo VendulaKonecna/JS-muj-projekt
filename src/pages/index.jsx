407ca766f8d0f4aa3b2796d1870e8ad6bf1b01dc
Vendula Konečná <VendaKonecna@gmail.com>
2025-04-28 15:35:50 +0200

    commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,83 @@
 import { render } from '@czechitas/render';
+import { Header } from '../components/Header/Header';
+import { Plysaci } from '../components/Plysaci'
+
 import '../global.css';
 import './index.css';
+import './super.css';
+//import { data } from './seznam.json';
+
+const Odkazy = () => {
+  return (
+    <ul>
+      <li><a href="#">Odkaz 1</a></li>
+      <li><a href="#">Odkaz 2</a></li>
+      <li><a href="#">Odkaz 3</a></li>
+      <li><a href="#">Odkaz 4</a></li>
+    </ul>
+  )
+}
 
 const Footer = () => {
   return (
     <footer>
-    <p>© 2024 Czechitas, z. ú.</p>
+      <p>Czechitas, nová patička upgradovaná</p>
     </footer>
   );
-};
-  
+}
+
+/*
+const Plysaci = () => {
+
+  const Ctirad = () => {
+    const plysakName = "Ctirad"
+    const plysakImg = "https://kodim.cz/cms/assets/czechitas/js2/lekce/sablony-komponenty/cv-komponenty/plysaci/mouse.jpg"
+    const plysakText = "Ctirad tráví svůj čas v blízkosti lednice a s očekáváním pozoruje její bílé dveře."
+
+    return (
+      <div class="plushy">
+        <img src={plysakImg} />
+        <strong>{plysakName}</strong>
+        <p>{plysakText}</p>
+      </div>
+    )
+  }
+
+  const Silvestr = () => {
+    const plysakName = "Silvestr"
+    const plysakImg = "https://kodim.cz/cms/assets/czechitas/js2/lekce/sablony-komponenty/cv-komponenty/plysaci/elephant.jpg"
+    const plysakText = "Silvestr rád pozoruje dění za oknem a upřímně se usmívá na všechno kolemjdoucí."
+
+    return (
+      <div>
+        <img src={plysakImg} />
+        <strong>{plysakName}</strong>
+        <p>{plysakText}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Ctirad />
+      <Silvestr />
+    </div>
+  )
+}
+
+*/
+
+
 document.querySelector('#root').innerHTML = render(
   <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace-upraveno</h1>
-    </header>
+    <Header title="Můj titulek stránky" user="Filip Procházka" />
+
     <main>
-      <p>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
+      <p>Moje naklonovaná stránka.</p>
+      <br />
+      <Odkazy />
+      <Plysaci />
     </main>
     <Footer />
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
   </div>
 );
